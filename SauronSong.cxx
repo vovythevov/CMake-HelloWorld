fc3bb9f67806ac7c5e1b4e8b1a9c232578430bfa
Johan Andruejol <johan.andruejol@kitware.com>
2012-08-13 10:32:23 -0400

    Init repository - It's alive ! Alive !

---
+++ SauronSong.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main( int argc, char * argv[] )
+{
+  std::cout<<"Three Rings for the Elven-kings under the sky,"<<std::endl
+           <<"Seven for the Dwarf-lords in their halls of stone,"<<std::endl
+           <<"Nine for Mortal Men doomed to die,"<<std::endl
+           <<"One for the Dark Lord on his dark throne,"<<std::endl
+           <<"In the Land of Mordor where the Shadows lie,"<<std::endl
+           <<"One ring to rule them all, one ring to find them,"<<std::endl
+           <<"One ring to bring them all and in the darkness bind them"<<std::endl
+           <<"In the Land of Mordor where the Shadows lie."<<std::endl;
+           
+  return EXIT_SUCCESS;
+}
